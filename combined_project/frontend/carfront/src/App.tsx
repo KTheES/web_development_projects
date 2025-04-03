45353189866ce198721dcc5e6d7aae6ce5a8243b
KTheES <keunsu2889@gmail.com>
2025-04-03 21:32:38 +0900

    feat:20250403 carlist get request

--- combined_project/frontend/carfront/src/App.tsx
+++ combined_project/frontend/carfront/src/App.tsx
@@ -3,20 +3,25 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import CssBaseline from '@mui/material/CssBaseline'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Carlist from './components/Carlist'
 
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
     <Container maxWidth="xl">
       <CssBaseline />
       <AppBar position='static'>
-        <Toolbar>ㅂㅂ
+        <Toolbar>
           <Typography variant='h6'>
             Car Shop
           </Typography>
         </Toolbar>
       </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Carlist />
+      </QueryClientProvider>
     </Container>
   )
 }
